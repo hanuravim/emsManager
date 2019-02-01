465c7a32fe4ab1a287fc438cb04f5eff9ccf6e88
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-02-01 17:32:31 -0600

    Add files via upload

---
+++ Configure_MountFileShare-DisableDefender_Updates.ps1
@@ -0,0 +1,20 @@
+﻿$SAKey
+$SAName
+$AzureFileShareName
+
+#CONFIGURE AZURE FILE SHARE ON PORTAL
+Import-Module Azure
+$storageContext = New-AzureStorageContext -StorageAccountName $SAName -StorageAccountKey $SAKey
+$storageContext |  New-AzureStorageShare -Name $AzureFileShareName
+
+#MOUNT AZURE FILE SHARE
+$acctKey = ConvertTo-SecureString -String $SAKey -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\$SAName", $acctKey
+New-PSDrive -Name Z -PSProvider FileSystem -Root "\\$SAName.file.core.windows.net\$AzureFileShareName" -Credential $credential -Persist 
+
+#DISABLE WINDOWS DEFENDER
+Set-MpPreference -DisableRealtimeMonitoring $true
+#################################################
+#DISABLE AUTO UPDATES
+Stop-Service -Name "wuauserv" -Force
+#################################################
