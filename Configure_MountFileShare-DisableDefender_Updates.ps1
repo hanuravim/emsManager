2b33d3cbe28d3889fbb3b2e3dd7812342b22a0bb
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-02-02 17:50:17 -0600

    Update Configure_MountFileShare-DisableDefender_Updates.ps1

--- Configure_MountFileShare-DisableDefender_Updates.ps1
+++ Configure_MountFileShare-DisableDefender_Updates.ps1
@@ -6,18 +6,18 @@ Param (
 )
 
 #CONFIGURE AZURE FILE SHARE ON PORTAL
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module Azure -Confirm:$False
-Import-Module Azure
-$storageContext = New-AzureStorageContext -StorageAccountName $SAName -StorageAccountKey $SAKey
-$storageContext |  New-AzureStorageShare -Name $AzureFileShareName
+#Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+#Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+#Install-Module Azure -Confirm:$False
+#Import-Module Azure
+#$storageContext = New-AzureStorageContext -StorageAccountName $SAName -StorageAccountKey $SAKey
+#$storageContext |  New-AzureStorageShare -Name $AzureFileShareName
 #Start-Sleep 60
 
 #MOUNT AZURE FILE SHARE
 $acctKey = ConvertTo-SecureString -String $SAKey -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\$SAName", $acctKey
-New-PSDrive -Name X -PSProvider FileSystem -Root "\\$SAName.file.core.windows.net\$AzureFileShareName" -Credential $SAKey -Persist 
+New-PSDrive -Name X -PSProvider FileSystem -Root "\\$SAName.file.core.windows.net\$AzureFileShareName" -Credential $credential -Persist 
 
 #DISABLE WINDOWS DEFENDER
 #Set-MpPreference -DisableRealtimeMonitoring $true
