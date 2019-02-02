0d3488aad416ee2a4dffa59bf2c4e56fc0565a38
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-02-01 23:30:45 -0600

    Update Configure_MountFileShare-DisableDefender_Updates.ps1

--- Configure_MountFileShare-DisableDefender_Updates.ps1
+++ Configure_MountFileShare-DisableDefender_Updates.ps1
@@ -1,2 +1,5 @@
+$SAKey
+$SAName
+$AzureFileShareName
 $uri = 'https://s1events.azure-automation.net/webhooks?token=Ti4y9MmLrYUsKwkicjGBOn3SvU0QiADA9wsj47R4Wqo%3d'
 Invoke-RestMethod -Method POST -Uri $uri
