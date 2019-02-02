c7511f2e4308d3d2865d0c7159faf5d4376b89ce
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-02-02 00:23:36 -0600

    Update Configure_MountFileShare-DisableDefender_Updates.ps1

--- Configure_MountFileShare-DisableDefender_Updates.ps1
+++ Configure_MountFileShare-DisableDefender_Updates.ps1
@@ -1,5 +1,23 @@
 $SAKey
 $SAName
 $AzureFileShareName
-$uri = 'https://s1events.azure-automation.net/webhooks?token=Ti4y9MmLrYUsKwkicjGBOn3SvU0QiADA9wsj47R4Wqo%3d'
-Invoke-RestMethod -Method POST -Uri $uri
+
+#CONFIGURE AZURE FILE SHARE ON PORTAL
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module Azure -Confirm:$False
+Import-Module Azure
+$storageContext = New-AzureStorageContext -StorageAccountName $SAName -StorageAccountKey $SAKey
+$storageContext |  New-AzureStorageShare -Name $AzureFileShareName
+
+#MOUNT AZURE FILE SHARE
+$acctKey = ConvertTo-SecureString -String $SAKey -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\$SAName", $acctKey
+New-PSDrive -Name Z -PSProvider FileSystem -Root "\\$SAName.file.core.windows.net\$AzureFileShareName" -Credential $credential -Persist 
+
+#DISABLE WINDOWS DEFENDER
+Set-MpPreference -DisableRealtimeMonitoring $true
+#################################################
+#DISABLE AUTO UPDATES
+Stop-Service -Name "wuauserv" -Force
+#################################################
