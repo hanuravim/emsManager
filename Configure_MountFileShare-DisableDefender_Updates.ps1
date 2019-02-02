648351f7c7458150f7b7bbcb82de5a4896b25ad1
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-02-02 11:31:40 -0600

    Update Configure_MountFileShare-DisableDefender_Updates.ps1

--- Configure_MountFileShare-DisableDefender_Updates.ps1
+++ Configure_MountFileShare-DisableDefender_Updates.ps1
@@ -5,6 +5,7 @@ Param (
   [String]$AzureFileShareName
 
 )
+
 #CONFIGURE AZURE FILE SHARE ON PORTAL
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
@@ -12,16 +13,16 @@ Install-Module Azure -Confirm:$False
 Import-Module Azure
 $storageContext = New-AzureStorageContext -StorageAccountName $SAName -StorageAccountKey $SAKey
 $storageContext |  New-AzureStorageShare -Name $AzureFileShareName
-Start-Sleep 300
+#Start-Sleep 60
 
 #MOUNT AZURE FILE SHARE
-$acctKey = ConvertTo-SecureString -String $SAKey -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\$SAName", $acctKey
-New-PSDrive -Name Z -PSProvider FileSystem -Root "\\$SAName.file.core.windows.net\$AzureFileShareName" -Credential $credential -Persist 
+#$acctKey = ConvertTo-SecureString -String $SAKey -AsPlainText -Force
+#$credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\$SAName", $acctKey
+New-PSDrive -Name X -PSProvider FileSystem -Root "\\emswu2trprdemsstr001.file.core.windows.net\emsazrfileshare" -Credential $SAKey -Persist 
 
 #DISABLE WINDOWS DEFENDER
-Set-MpPreference -DisableRealtimeMonitoring $true
+#Set-MpPreference -DisableRealtimeMonitoring $true
 #################################################
 #DISABLE AUTO UPDATES
-Stop-Service -Name "wuauserv" -Force
+#Stop-Service -Name "wuauserv" -Force
 #################################################
