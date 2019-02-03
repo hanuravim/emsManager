988f1d0822b8968c610ebd285456b0a7f7b3feff
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-02-02 19:10:49 -0600

    Update ConfigureAzrFile.ps1

--- ConfigureAzrFile.ps1
+++ ConfigureAzrFile.ps1
@@ -4,5 +4,21 @@ Param (
   [String]$SAName,
   [String]$AzureFileShareName
 )
-#CONFIGURE AZURE FILE SHARE ON PORTAL
+#DISABLE WINDOWS DEFENDER
 Set-MpPreference -DisableRealtimeMonitoring $true
+
+#CONFIGURE AZURE FILE SHARE ON PORTAL
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module Azure -Confirm:$False
+Import-Module Azure
+$storageContext = New-AzureStorageContext -StorageAccountName $SAName -StorageAccountKey $SAKey
+$storageContext |  New-AzureStorageShare -Name $AzureFileShareName
+
+#MOUNT AZURE FILE SHARE
+$acctKey = ConvertTo-SecureString -String $SAKey -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\$SAName", $acctKey
+New-PSDrive -Name X -PSProvider FileSystem -Root "\\$SAName.file.core.windows.net\$AzureFileShareName" -Credential $credential -Persist 
+
+#DISABLE AUTO UPDATES
+Stop-Service -Name "wuauserv" -Force
