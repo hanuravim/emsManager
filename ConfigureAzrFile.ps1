ac7c4ac609d32710c2f1d1931704f2c9d3cb5cb7
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-02-03 01:17:16 -0600

    Update ConfigureAzrFile.ps1

--- ConfigureAzrFile.ps1
+++ ConfigureAzrFile.ps1
@@ -7,6 +7,21 @@ Param (
 #DISABLE WINDOWS DEFENDER
 Set-MpPreference -DisableRealtimeMonitoring $true
 
+#INITIAIZE ADI VOLUME
+$disks = Get-Disk | Where partitionstyle -eq 'raw' | sort number
+$count = 0
+    $letter = "F"
+    foreach ($disk in $disks) {
+        $drive = $letter.ToString()
+        $disk | 
+        Initialize-Disk -PartitionStyle MBR -PassThru |
+        New-Partition -UseMaximumSize -DriveLetter $drive |
+        Format-Volume -FileSystem NTFS -NewFileSystemLabel $letter -Confirm:$false -Force
+    $count++
+    }
+$ADI = Get-CimInstance -Class Win32_Volume -Filter "driveletter='F:'"
+Set-CimInstance -InputObject $ADI -Arguments @{Label="ADI"}
+
 #CONFIGURE AZURE FILE SHARE ON PORTAL
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
