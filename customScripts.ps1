cf4dc252b2de8f1e240dd05c6a1f1c11700b4f1b
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-02-08 10:50:20 -0600

    Update customScripts.ps1

--- customScripts.ps1
+++ customScripts.ps1
@@ -7,3 +7,25 @@ Param (
 #DISABLE UAC
 Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -Value 0
 shutdown /r
+
+#DISABLE WINDOWS DEFENDER
+Set-MpPreference -DisableRealtimeMonitoring $true
+
+#INITIAIZE ADI VOLUME
+$disks = Get-Disk | Where partitionstyle -eq 'raw' | sort number
+$count = 0
+    $letter = "F"
+    foreach ($disk in $disks) {
+        $drive = $letter.ToString()
+        $disk | 
+        Initialize-Disk -PartitionStyle MBR -PassThru |
+        New-Partition -UseMaximumSize -DriveLetter $drive |
+        Format-Volume -FileSystem NTFS -NewFileSystemLabel $letter -Confirm:$false -Force
+    $count++
+    }
+$ADI = Get-CimInstance -Class Win32_Volume -Filter "driveletter='F:'"
+Set-CimInstance -InputObject $ADI -Arguments @{Label="ADI"}
+
+#ENABLE UAC
+Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -Value 1
+shutdown /r
