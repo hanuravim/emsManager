966d0324db423274493b97f63f3ee2350cd38b69
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-02-08 16:55:08 -0600

    Update customScripts.ps1

--- customScripts.ps1
+++ customScripts.ps1
@@ -29,9 +29,6 @@ Set-CimInstance -InputObject $ADI -Arguments @{Label="ADI"}
 #DISABLE AUTO UPDATES
 sc.exe stop wuauserv
 
-#REMOTE DESKTOP GATEWAY
-Install-WindowsFeature -Name 'RDS-Gateway' -IncludeAllSubFeature
-
 #ENABLE UAC
 Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -Value 1
 shutdown /r
