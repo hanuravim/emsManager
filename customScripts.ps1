46dde7e9ef131aedab1dea903805fa45acfaabe6
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-02-08 10:18:01 -0600

    Update customScripts.ps1

--- customScripts.ps1
+++ customScripts.ps1
@@ -7,3 +7,6 @@ Param (
 #DISABLE UAC
 Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -Value 0
 shutdown /r
+
+#DISABLE WINDOWS DEFENDER
+Set-MpPreference -DisableRealtimeMonitoring $true
