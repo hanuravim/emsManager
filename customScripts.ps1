156d609a808469618dae8afa676069051c8d234c
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-02-08 16:28:55 -0600

    Update customScripts.ps1

--- customScripts.ps1
+++ customScripts.ps1
@@ -32,11 +32,6 @@ sc.exe stop wuauserv
 #REMOTE DESKTOP GATEWAY
 Install-WindowsFeature -Name 'RDS-Gateway' -IncludeAllSubFeature
 
-#MOUNT AZURE FILE SHARE
-$acctKey = ConvertTo-SecureString -String $SAKey -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\$SAName", $acctKey
-New-PSDrive -Name X -PSProvider FileSystem -Root "\\$SAName.file.core.windows.net\$AzureFileShareName" -Credential $credential
-
 #ENABLE UAC
 Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -Value 1
 shutdown /r
