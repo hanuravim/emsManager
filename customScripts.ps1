41ca49e9ff31570ded31348e8d77c95ebb7721cd
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-02-09 01:15:50 -0600

    Update customScripts.ps1

--- customScripts.ps1
+++ customScripts.ps1
@@ -1,65 +1,45 @@
-#DISABLE UAC
-Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -Value 0
-shutdown /r
-function Connect-AzureFileShare
-{
-    [CmdletBinding()]
-    Param(
-        
-        [String]$SAName = 'emscutrdevemsstr001',
-        [String]$AzureFileShareName = 'emsazrfileshare',
-        [ValidateLength(1,2)]
-        [String]$DriveLetter = 'Z'
-    )
+Param (
+  [Parameter()]
+  [String]$SAKey,
+  [String]$SAName,
+  [String]$AzureFileShareName
+)
+#DISABLE WINDOWS DEFENDER
+Set-MpPreference -DisableRealtimeMonitoring $true
+
+#DISABLE AUTO UPDATES
+sc.exe stop wuauserv
+sc.exe config wuauserv start= disabled
+
+#REMOTE DESKTOP GATEWAY
+Install-WindowsFeature -Name 'RDS-Gateway' -IncludeAllSubFeature
+
+#INITIAIZE ADI VOLUME
+$disks = Get-Disk | Where partitionstyle -eq 'raw' | sort number
+$count = 0
+    $letter = "F"
+    foreach ($disk in $disks) {
+        $drive = $letter.ToString()
+        $disk | 
+        Initialize-Disk -PartitionStyle MBR -PassThru |
+        New-Partition -UseMaximumSize -DriveLetter $drive |
+        Format-Volume -FileSystem NTFS -NewFileSystemLabel $letter -Confirm:$false -Force
+    $count++
+    }
+$ADI = Get-CimInstance -Class Win32_Volume -Filter "driveletter='F:'"
+Set-CimInstance -InputObject $ADI -Arguments @{Label="ADI"}
+
+#CONFIGURE AZURE FILE SHARE ON PORTAL
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module Azure -Confirm:$False
 Import-Module Azure
+$StorageAccount = Get-AzureRMStorageAccount | ?{$_.StorageAccountName -eq "$SAName"}
+$storageContext = New-AzureStorageContext -StorageAccountName $SAName -StorageAccountKey $SAKey
+$storageContext |  New-AzureStorageShare -Name $AzureFileShareName
 
-    $StorageAccount = Get-AzureRMStorageAccount | ?{$_.StorageAccountName -eq "$StorageAccountName"}
-    If ($StorageAccountName -eq $StorageAccount.StorageAccountName)
-        {
-
-        Write-Verbose "StorageAccountName resolves correctly"
-        Write-Debug "Entered [$StorageAccountName] and found [$($StorageAccount.StorageAccountName)]"
-        } else {
-            Write-Output "Storage Account Not Found"
-            Break
-        }
-
-    # Validate DriveLetter and correct if needed
-    If ($DriveLetter.Length -gt 1)
-    {
-        Write-Verbose "DriveLetter.Length was Greater than 1"
-        If (!($DriveLetter.EndsWith(':')))
-        {
-            Write-Verbose "DriveLetter[1] was not ':'"
-            $DriveLetter[1] = ":"
-        }
-    } else {
-
-        $DriveLetter = $DriveLetter +":"
-    }
-
-    # Check DriveLetter is not currently in use 
-    Write-Verbose "Checking drive letter is not in use"
-    $DriveExists = (Get-PSDrive -Name $DriveLetter[0] -ErrorAction SilentlyContinue)
-    if ($DriveExists)
-    {
-        Write-Output "DriveLetter $($DriveLetter[0]) is in use, please select another"
-        break
-    }
-    $StorageAccountKey = (Get-AzureRmStorageAccountKey $StorageAccount.StorageAccountName -ResourceGroupName $StorageAccount.ResourceGroupName)[0].Value
-    $StorageContext = (New-AzureStorageContext -StorageAccountName $StorageAccount.StorageAccountName -StorageAccountKey $StorageAccountKey)
-    $UNCPath = "\\" +($StorageAccount.PrimaryEndpoints.File).SubString(8).TrimEnd('/')
-    $Share = (Get-AzureStorageShare -Context $StorageContext)
-    $CombinedPath = (Join-Path $UNCPath -ChildPath $ShareName)
-    $UserName = "/u:" +$StorageAccount.StorageAccountName
-    Write-Verbose "Executing: net use $DriveLetter $CombinedPath $UserName $StorageAccountKey"
-    
-    Return net use $DriveLetter $CombinedPath $UserName $StorageAccountKey
-}
-Connect-AzureFileShare
-#ENABLE UAC
-Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -Value 1
-shutdown /r
+$UNCPath = "\\" +($StorageAccount.PrimaryEndpoints.File).SubString(8).TrimEnd('/')
+$Share = (Get-AzureStorageShare -Context $StorageContext)
+$CombinedPath = (Join-Path $UNCPath -ChildPath $AzureFileShareName)
+$UserName = "/u:" +$StorageAccount.StorageAccountName
+Return net use $DriveLetter $CombinedPath $UserName $StorageAccountKey
