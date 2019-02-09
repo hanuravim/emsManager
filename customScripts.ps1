d8e2fc3401e0e4e8243d3cdb4a1ff589c5d8d094
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-02-08 21:13:29 -0600

    Update customScripts.ps1

--- customScripts.ps1
+++ customScripts.ps1
@@ -1,14 +1,24 @@
 Param (
   [Parameter()]
-  [String]$SAKey,
-  [String]$SAName,
-  [String]$AzureFileShareName
+  [String]$SAKey = 'IXX990bdk/6NaZMtFAMMJyrjCFSVmcwIWkQmqli90yAAXfLCnCWBgsQMeOJe+Fm+pCt25i7yLs6m/ej4fXD7mg==',
+  [String]$SAName = 'emscutrdevemsstr001',
+  [String]$AzureFileShareName = 'emsazrfileshare'
 )
+$path = $env:APPDATA + '\persistvar.txt'
+$SAKey,$SAName,$AzureFileShareName | Out-File -FilePath $path
+$vars = Get-Content -Path $path
+$SAKey = $vars[0];$SAName = $vars[1];$AzureFileShareName = $vars[2]
+
+#DISABLE UAC
+$UAC = Get-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA # -Value 0
+if($UAC.EnableLUA -eq '1'){ Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -Value 0;Restart-Computer -Wait}
+
 #DISABLE WINDOWS DEFENDER
 Set-MpPreference -DisableRealtimeMonitoring $true
 
 #DISABLE AUTO UPDATES
 sc.exe stop wuauserv
+sc.exe config wuauserv start= disabled
 
 #REMOTE DESKTOP GATEWAY
 Install-WindowsFeature -Name 'RDS-Gateway' -IncludeAllSubFeature
@@ -28,15 +38,6 @@ $count = 0
 $ADI = Get-CimInstance -Class Win32_Volume -Filter "driveletter='F:'"
 Set-CimInstance -InputObject $ADI -Arguments @{Label="ADI"}
 
-#CONFIGURE AZURE FILE SHARE ON PORTAL
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module Azure -Confirm:$False
-Import-Module Azure
-$storageContext = New-AzureStorageContext -StorageAccountName $SAName -StorageAccountKey $SAKey
-$storageContext |  New-AzureStorageShare -Name $AzureFileShareName
-
-#MOUNT AZURE FILE SHARE
-$acctKey = ConvertTo-SecureString -String $SAKey -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\$SAName", $acctKey
-New-PSDrive -Name X -PSProvider FileSystem -Root "\\$SAName.file.core.windows.net\$AzureFileShareName" -Credential $credential
+#ENABLE UAC
+Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -Value 1
+shutdown /r
